b8b9d86f924cea88af6f90ddfc79e48f4cb23a0d
heribender <heribender@web.de>
2015-10-23 18:04:15 +0200

    configuration anonymized mail_template.html: some modifications. Added a bottom line "do not reply..."

--- configuration.py
+++ configuration.py
@@ -21,19 +21,22 @@ class Smtp(object):
         self.user = user
         self.password = password
 
+# Simple SMTP host, e.g. postfix on the local system, without login		
 smtp = Smtp(host = "localhost")
+# Remote SMTP host, SSL connection, Login:
+#smtp = Smtp(host = "smtp.remote.host", with_ssl= True, port = 465, user = "username", password = "userpassword" )
 
 # The email address of the digest sender. Best you chose a real address, as users might respond to it.
-sender = "zusammenfassung@nena1.ch"
+sender = "noreply@my.oxwall.xy"
 
 # Path to the state file that will be updated with a checkpoint everytime the application has run.
 # The path can be either absolute or relative.
 # Each line in the state file is a valid JSON-encoded object.
-state_path = "/home/marko/digest.state.json"
+state_path = "/opt/oxdigest/state.json"
 
 # Path to the log. You can specify either a relative or an absolute path.
 # Each line is a valid JSON-encoded object.
-log_path = "/home/marko/digest.log.json"
+log_path = "/var/log/oxdigest/log.json"
 
 # Email address of the administrator. Shows whenever a mail client can not display HTML and
 # at the end of the digest mail to direct the user for opting out.
@@ -48,7 +51,7 @@ send_to_all_oxwall_users = False
 # allows you to include users who are not registered in Oxwall, but who would like
 # to receive the email. In case you are not sending an email to all users (see send_to_all_oxwall_users),
 # you can cherry-pick to whom the digest should be sent.
-additional_recipients = ["marko.ristin@gmail.com"]
+additional_recipients = ["my.test.recepient@host.com"]
 
 # list of email addresses which are excluded from the digest recipient list.
 excluded_recipients = []
@@ -58,4 +61,4 @@ excluded_recipients = []
 exclude_content_before = datetime.datetime(2012,10,1)
 
 # the url prefix appended to all Oxwall links (e.g., to forum posts or blog posts)
-url_prefix = "http://oxwall.nena1.ch"
+url_prefix = "http://my.oxwall.xy"
